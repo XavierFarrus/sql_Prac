554f427475142b384918a987e3ce2e9c8122fc7d
Erik <erikmuar91@gmail.com>
2025-02-06 09:36:26 +0100

    Add files via upload

--- Practica2.sql
+++ Practica2.sql
@@ -43,6 +43,21 @@ ORDER BY a.city LIMIT 1 OFFSET 4;
 -- Quin és el nom del jugador mostrat en la primera fila
 
 
+INSERT INTO answer (IDquestion, answer_value, sql_query_used) VALUES ('5', 'Evan', 'SELECT p.name, p. surname
+FROM person AS p 
+JOIN player AS pl ON p.IDCard = pl.IDCard 
+JOIN draft_player_franchise AS dpf ON dpf.IDCardPlayer = pl.IDCard
+WHERE dpf.position IN (1,2,3) AND DraftYear = 2020
+ORDER BY p.surname DESC LIMIT 1;');
+
+SELECT p.name, p. surname
+FROM person AS p 
+JOIN player AS pl ON p.IDCard = pl.IDCard 
+JOIN draft_player_franchise AS dpf ON dpf.IDCardPlayer = pl.IDCard
+WHERE dpf.position IN (1,2,3) AND DraftYear = 2020
+ORDER BY p.surname DESC LIMIT 1;
+
+
 -- 6
 SELECT p.Name
 FROM person p
@@ -50,3 +65,24 @@ JOIN player pl ON p.IDCard  = pl.IDCard
 WHERE p.BirthDate <= '1980-03-01' AND p.Surname = 'Lue'
 ORDER BY p.BirthDate DESC
 LIMIT 1;
+
+
+-- 8 Tenim guardat els colors dels seients de tots els estadis. Retorna quants seients blaus hi ha en total.
+
+INSERT INTO answer (IDquestion, answer_value, sql_query_used) VALUES ('8', '36402', 'SELECT COUNT(*) 
+FROM seat AS s 
+WHERE Color = 'blue'');
+
+SELECT COUNT(*) 
+FROM seat AS s 
+WHERE Color = 'blue';
+
+
+-- 9 Retorna la mitjana de seients (arrodonint sense decimals) per color d’entre tots els estadis. Quina es la mitjana dels platejats? 
+
+SELECT s.Color, ROUND(COUNT(*) / COUNT(DISTINCT s.ArenaName)) AS average_seats
+FROM seat AS s
+GROUP BY s.Color;
+
+-- 10 Quin és el nom de l’equip que ha guanyat més partits com a visitant?
+
